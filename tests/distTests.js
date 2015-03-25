5e9bd5005ffa3d869f2d4723f8eaa1cfd6797a88
unbornchikken <gabor.mezo@outlook.com>
2015-03-25 17:00:02 +0100

    dist ok

---
+++ tests/distTests.js
@@ -0,0 +1,27 @@
+"use strict";
+/* global describe,id */
+
+var Bluebird = require("bluebird");
+var rimraf = Bluebird.promisify(require("rimraf"));
+var dist = require("../lib/dist");
+var assert = require("assert");
+
+var testDownload = process.env.TEST_DWONLOAD === "1";
+
+describe("dist", function () {
+    if (testDownload) {
+        it("should download dist files if needed", function (done) {
+            this.timeout(30000);
+            console.log("Internal path: " + dist.internalPath);
+            rimraf(dist.internalPath)
+                .then(function () {
+                    assert(dist.downloaded === false);
+                    return dist.ensureDownloaded();
+                })
+                .then(function () {
+                    assert(dist.downloaded);
+                })
+                .nodeify(done);
+        });
+    }
+});
