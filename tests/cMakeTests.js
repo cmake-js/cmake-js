609e9ee4f06b37db4add3a9849a4724ebe521793
unbornchikken <gabor.mezo@outlook.com>
2015-03-26 22:50:34 +0100

    builds on linux

--- tests/cMakeTests.js
+++ tests/cMakeTests.js
@@ -5,14 +5,15 @@ var assert = require("assert");
 var CMake = require("../lib/cMake");
 var which = require("which");
 var _ = require("lodash");
+var path = require("path");
 
 describe("CMake", function () {
-    it("shoud provide list of generators", function (done) {
-        CMake.getGenerators()
-            .then(function (gens) {
-                assert(_.isArray(gens));
-                assert(gens.length > 0);
-                assert.equal(gens.filter(function (g) { return g.length; }).length, gens.length);
+    it("shoud rebuild prototype", function (done) {
+        var cmake = new CMake({
+            directory: path.resolve(path.join(__dirname, "../prototype"))
+        });
+        cmake.rebuild()
+            .then(function() {
             })
             .nodeify(done);
     });
