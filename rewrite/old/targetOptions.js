745c3e8af100bbfdafed2189bd49fd829cc4ab29
Julian Waller <git@julusian.co.uk>
2024-01-22 23:05:16 +0000

    wip: copy old code to allow for gradual deletion upon reimplementation

---
+++ rewrite/old/targetOptions.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const environment = require('./environment')
+
+class TargetOptions {
+	get arch() {
+		return this.options.arch || environment.arch
+	}
+	get isX86() {
+		return this.arch === 'ia32' || this.arch === 'x86'
+	}
+	get isX64() {
+		return this.arch === 'x64'
+	}
+	get isArm() {
+		return this.arch === 'arm'
+	}
+	get runtime() {
+		return this.options.runtime || environment.runtime
+	}
+	get runtimeVersion() {
+		return this.options.runtimeVersion || environment.runtimeVersion
+	}
+
+	constructor(options) {
+		this.options = options || {}
+	}
+}
+
+module.exports = TargetOptions
