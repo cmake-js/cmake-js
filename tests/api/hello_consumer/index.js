6a68eef143b0acb27c2baa0c60f78b563560b264
StoneyDSP <nathanjhood@googlemail.com>
2024-02-03 07:01:34 +0100

    added 'hello_with_types' example cmake-js project

--- tests/api/hello_consumer/index.js
+++ tests/api/hello_consumer/index.js
@@ -6,3 +6,6 @@
 const addon = require('@vendor/hello');
 
 console.log(addon.hello())
+
+// If I swap this package for '@vendor/hello_with_types',
+// my intellisense engine explains the functions to me :)
