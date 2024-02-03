66c69e08cea0638e0b22b14e869337924cee5f53
StoneyDSP <nathanjhood@googlemail.com>
2024-02-03 07:35:00 +0100

    comment on types usage

--- tests/api/hello_consumer/index.js
+++ tests/api/hello_consumer/index.js
@@ -7,5 +7,8 @@ const addon = require('@vendor/hello');
 
 console.log(addon.hello())
 
-// If I swap this package for '@vendor/hello_with_types',
-// my intellisense engine explains the functions to me :)
+// If I swap my package.json dependency for
+// '@vendor/hello_with_types' and do
+// 'require('@vendor/hello_with_types')' here instead,
+// then my intellisense engine explains the
+// functions to me :)
